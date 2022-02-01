c85114877337e6d80e03653cdf1a0aa2b591e1bd
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-01 14:20:23 +0100

    controller spaces with defs for index, new and create / view new for spaces / installed gem EnumHelp to make type accessible in simple_form

---
+++ app/controllers/spaces_controller.rb
@@ -0,0 +1,31 @@
+class SpacesController < ApplicationController
+  def index
+    @spaces = policy_scope(Space)
+  end
+
+  def new
+    @space = Space.new
+    authorize @space
+  end
+
+  def create
+    @space = Space.new(space_params)
+    @space.user = current_user
+    authorize @space
+    if @space.save
+      redirect_to space_path(@space), notice: "Reading space was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def show
+    authorize @space
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:title, :description, :type, )
+  end
+end
