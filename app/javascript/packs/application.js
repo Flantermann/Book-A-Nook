23409e6c1e7da7fe6e94d4a50b4aa0be922f1b3f
Isabel Eberle <i.eberle926@googlemail.com>
2022-02-08 18:59:40 +0100

    flatpickr status quo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,15 +23,14 @@ import "bootstrap";
 import flatpickr from 'flatpickr';
 import'flatpickr/dist/flatpickr.min.css'
 
-flatpickr(".datepicker", {
-  altInput: true,
-  enableTime: true
-});
-
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  flatpickr(".datepicker", {
+    altInput: true,
+    enableTime: true
+  });
 });
