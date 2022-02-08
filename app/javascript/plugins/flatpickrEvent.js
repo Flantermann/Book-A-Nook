ddbb16d94cf23ad3f707ef9c1dc0a1e021b44401
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-08 22:37:49 +0100

    more work on booking show and index pages

--- app/javascript/plugins/flatpickrEvent.js
+++ app/javascript/plugins/flatpickrEvent.js
@@ -1,15 +1,15 @@
-const flatpickrEvent = () => {
-  console.log('testing')
-  const flatpickrInputs = document.querySelectorAll('.datepicker.input')
-  console.log(flatpickrInputs)
-  if(flatpickrInputs){
-    console.log('inside if statement')
-    flatpickrInputs[0].addEventListener('change', () => {
-        console.log('change')
-        console.log(event.currentTarget.value)
-        flatpickrInputs[1].value = event.currentTarget.value
-      })
-  }
-}
+// const flatpickrEvent = () => {
+//   console.log('testing')
+//   const flatpickrInputs = document.querySelectorAll('.datepicker.input')
+//   console.log(flatpickrInputs)
+//   if(flatpickrInputs){
+//     console.log('inside if statement')
+//     flatpickrInputs[0].addEventListener('change', () => {
+//         console.log('change')
+//         console.log(event.currentTarget.value)
+//         flatpickrInputs[1].value = event.currentTarget.value
+//       })
+//   }
+// }
 
-export {flatpickrEvent}
+// export {flatpickrEvent}
