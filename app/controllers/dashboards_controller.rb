d01af1c573a461d957b9b036ac502fbd139ce8cc
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-10 13:57:10 +0100

    change dashboard index page to onöy show booking requests headline when there are bookings. add variable to dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,11 @@ class DashboardsController < ApplicationController
   def index
     @my_bookings = current_user.bookings
     @my_rentals = current_user.spaces
+    @bookings_of_rentals = []
+      @my_rentals.each do |rental|
+        rental.bookings.each do |booking|
+          @bookings_of_rentals << booking
+        end
+      end
   end
 end
